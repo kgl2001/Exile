2f7fc83a52ffe5f3e69c65d7b0996c5674477a2e
Ken Lowe <36017029+kgl2001@users.noreply.github.com>
2025-01-27 01:28:18 +0000

    Reposition constants

--- ExileSR.asm
+++ ExileSR.asm
@@ -1,11 +1,11 @@
+;constants
+NOP = 1
+
 ; SWRAM_FE6x: Options are TRUE, FALSE or NOP
 ; TRUE will include SWRAM FE6x code. NOP will replace SWRAM FE6x code with NOP instructions
 SWRAM_FE6x = TRUE
 
 ; KEEP_FINAL_JMP: Options are TRUE or FALSE
 KEEP_FINAL_JMP = FALSE
-
-;constants
-NOP = 1
 
 include "ExileSR_main.asm"
